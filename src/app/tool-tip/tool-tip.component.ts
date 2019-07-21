9e2daaf7bd73c5faabd22647149d30074fd7809a
jyoti1904 <jyotisharma88js@gmail.com>
2019-07-21 16:54:00 -0400

    css file changed to scss file _vars.css file added for declaring varaiables

--- src/app/tool-tip/tool-tip.component.ts
+++ src/app/tool-tip/tool-tip.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-tool-tip',
   templateUrl: './tool-tip.component.html',
-  styleUrls: ['./tool-tip.component.css']
+  styleUrls: ['./tool-tip.component.scss']
 })
 export class ToolTipComponent implements OnInit {
 
